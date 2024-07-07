041125a9f0ef8bc3f47316d60b91c07ab6c5794c
israelcvb <israelcvb21@gmail.com>
2024-07-07 17:58:09 -0300

    Criei a lista de produtos e novo produto.

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
-    path('website/', views.home, name='home')
+    path('website/', views.home, name='home'),
+    path('produtos/novo', views.CreateProductView.as_view(), name='new_product'),
+    path('produtos/', views.ListProductView.as_view(), name='products'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.views.generic import CreateView, ListView
+
+from .models import Product
 
 # Create your views here.
 def home(request):
     return HttpResponse("Olá, estamos começando a construção do WebApp Quitanda da Dona Maria!")
+
+
+class CreateProductView(CreateView):
+    model = Product
+    fields = ['barcode', 'name', 'description', 'price']
+
+class ListProductView(ListView):
+    model = Product
+    context_object_name = 'products'
